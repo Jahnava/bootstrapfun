409ff33138522ad81c4aa8402ae77c7a899f820e
jahnava <jahnavalmt@gmail.com>
2017-11-19 16:45:07 -0500

    added caption. still want to tweak location. also, want to fix js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */
-$('.carousel').carousel({
+$('#mycarousel').carousel({
     pause: "false"
     /* Change to true to make it paused when your mouse cursor enter the background */
+});
+
+// set time interval on slides
+$('#mycarousel').carousel({
+  interval: 1000
 });
