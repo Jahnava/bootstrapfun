53a87285ffe2b892a5b97c012c251326870c2fbf
jahnava <jahnavalmt@gmail.com>
2017-11-20 19:31:49 -0500

    edited caption

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-$('#mycarousel').carousel({
-    pause: "false"
-    /* Change to true to make it paused when your mouse cursor enter the background */
-});
-
-// set time interval on slides
-$('#mycarousel').carousel({
-  interval: 1000
-});
+// $('#mycarousel').carousel({
+//     pause: "false"
+//     /* Change to true to make it paused when your mouse cursor enter the background */
+// });
+//
+// // set time interval on slides
+// $('#mycarousel').carousel({
+//   interval: 1000
+// });
